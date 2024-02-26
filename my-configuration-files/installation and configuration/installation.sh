4c34f388b979e59d07e39153ee2ef476c51e6886
Antonio Pedro <86069266+antonio-pedro-alves-da-silva@users.noreply.github.com>
2024-02-26 13:32:08 -0300

    Update installation.sh

--- my-configuration-files/installation and configuration/installation.sh
+++ my-configuration-files/installation and configuration/installation.sh
@@ -1,45 +1,53 @@
-apt update
-add-apt-repository ppa:apt-fast/stable
-apt install apt-fast -y
+
+# download chromoe
+
+#1.generate new ssh key
+ssh-keygen -t ed25519 -C "antoniopedroalvesdasilva.apads@gmail.com"
+#2.copy the ssh key
+xclip -sel clip < ~/.ssh/id_ed25519.pub
+sudo apt-fast install tilix
+
+sudo apt update
+sudo add-apt-repository ppa:apt-fast/stable
+sudo apt install apt-fast -y
 
 git clone --depth=1 https://github.com/AdnanHodzic/auto-cpufreq.git
 cd auto-cpufreq && sudo ./auto-cpufreq-installer
-auto-cpufreq --install
-auto-cpufreq --force=performance
+sudo auto-cpufreq --install
+sudo auto-cpufreq --force=performance
 
-apt-fast install curl -y
-apt-fast install snapd -y
+sudo apt-fast install curl -y
+sudo apt-fast install snapd -y
 # configuração do tmux(não terminado)
 
-apt-fast install tilix
 # configuração do zsh
 
-apt-fast install zsh -y
+sudo apt-fast install zsh -y
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/plugins/zsh-autosuggestions
 
-apt-fast install fonts-powerline -y
+sudo apt-fast install fonts-powerline -y
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/themes/powerlevel10k
 
 # instalaçao do i3
 
-apt-fast install feh -y
-apt-fast install xdotool -y
-apt-fast install nodejs -y
-apt-fast install flameshot -y
-apt-fast install i3 -y
-apt-fast install i3status -y
+sudo apt-fast install feh -y
+sudo apt-fast install xdotool -y
+sudo apt-fast install nodejs -y
+sudo apt-fast install flameshot -y
+sudo apt-fast install i3 -y
+sudo apt-fast install i3status -y
 
 
 # other packages 
-apt-fast install gparted -y
-apt-fast install ffmpeg -y
-apt-fast install pavucontrol -y
-apt-fast install thunar -y
-apt-fast install blueman -y
+sudo apt-fast install gparted -y
+sudo apt-fast install ffmpeg -y
+sudo apt-fast install pavucontrol -y
+sudo apt-fast install thunar -y
+sudo apt-fast install blueman -y
 
 download vscode 
 download apache
