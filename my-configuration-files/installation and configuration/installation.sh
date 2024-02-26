eb2e100d564fba96e288a8455435afc2d24c73e3
Antonio Pedro <86069266+antonio-pedro-alves-da-silva@users.noreply.github.com>
2024-02-26 13:32:34 -0300

    Update installation.sh

--- my-configuration-files/installation and configuration/installation.sh
+++ my-configuration-files/installation and configuration/installation.sh
@@ -1,11 +1,13 @@
 
-# download chromoe
+# download chrome
+
+sudo apt-fast install tilix
 
 #1.generate new ssh key
 ssh-keygen -t ed25519 -C "antoniopedroalvesdasilva.apads@gmail.com"
 #2.copy the ssh key
 xclip -sel clip < ~/.ssh/id_ed25519.pub
-sudo apt-fast install tilix
+
 
 sudo apt update
 sudo add-apt-repository ppa:apt-fast/stable
