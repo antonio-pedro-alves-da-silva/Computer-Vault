bb95aef6c2d4bf344426a2f90558a3bcf71b8215
antonio <antoniopedroalvesdasilva.apads@gmail.com>
2024-04-09 20:19:33 -0300

    new update

--- my-configuration-files/js-scripts/addToMdTable.cjs
+++ my-configuration-files/js-scripts/addToMdTable.cjs
@@ -41,7 +41,7 @@ function arrayToMdTable(ArrayList,FirstRow){
         SecondRow += "--|";
     });
 
-    let restOfTheLines = '';
+    let restOfTheLines = '|';
     ArrayList.foreach((v,i,a)=>{
 
     })
