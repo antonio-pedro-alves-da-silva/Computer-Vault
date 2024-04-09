5438e36b45805f1a775294026cadf55a5ab43ca1
antonio <antoniopedroalvesdasilva.apads@gmail.com>
2024-04-09 20:09:39 -0300

    new update

--- my-configuration-files/js-scripts/addToMdTable.cjs
+++ my-configuration-files/js-scripts/addToMdTable.cjs
@@ -38,7 +38,7 @@ function arrayToMdTable(Array,FirstRow){
 
     let SecondRow = '|';
     FirstRow.match(/|/gm).foreach((v,i,a)=>{
-        Seocond
+        
     });
 
     Array.foreach((v,i,a)=>{
