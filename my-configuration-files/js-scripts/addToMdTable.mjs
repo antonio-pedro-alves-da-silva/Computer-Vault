d3f285b7581a5166057f536417340837bcf24b9f
antonio <antoniopedroalvesdasilva.apads@gmail.com>
2024-04-09 14:57:17 -0300

    new update

--- my-configuration-files/js-scripts/addToMdTable.mjs
+++ my-configuration-files/js-scripts/addToMdTable.mjs
@@ -6,9 +6,9 @@ const season = inputFile.match(/(?<=_s)\d+(?=e\d+.)/)[0];
 const episode = inputFile.match(/(?<=_s\de)\d+(?=[.])/)[0];
 
 
-const { exec } = require("child_process");
-const fs = require("fs").promises; // Import fs promises for async/await
-const { log } = require("console");
+// const { exec } = require("child_process");
+// const fs = require("fs").promises; // Import fs promises for async/await
+// const { log } = require("console");
 import { markdownTable } from "markdown-table";
 
 
