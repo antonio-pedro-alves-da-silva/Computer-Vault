98a0e77a9d7b781a92ce183f9c2394cf7ead7962
antonio <antoniopedroalvesdasilva.apads@gmail.com>
2024-04-09 19:31:07 -0300

    new update

--- my-configuration-files/js-scripts/addToMdTable.cjs
+++ my-configuration-files/js-scripts/addToMdTable.cjs
@@ -37,7 +37,7 @@ async function generateSrtArray(File) {
 function arrayToMdTable(Array,FirstRow){
     FirstRow += "\n";
     let FirstRowCols = FirstRow.match(/|/gm).foreach((v,i,a)=>{
-        FirstRow += "|a|--|"
+        FirstRow += "|--|--|"
     });
 
     let table = `${FirstRow}`;
