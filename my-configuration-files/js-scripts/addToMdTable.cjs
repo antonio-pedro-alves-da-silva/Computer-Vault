512d61e619296ed3ec65cfa237043376f708fc93
antonio <antoniopedroalvesdasilva.apads@gmail.com>
2024-04-09 08:11:16 -0300

    new update

--- my-configuration-files/js-scripts/addToMdTable.cjs
+++ my-configuration-files/js-scripts/addToMdTable.cjs
@@ -12,13 +12,13 @@ const { log } = require("console");
 import {markdownTable} from 'markdown-table';
 
 async function generateSrtArray(File) {
+    let srtArray = [];
     try {
         const data = await fs.readFile(File, "utf8");
 
         let numberArray = data.match(/^\d+$/gm);
         let timeSrt = data.match(/\d\d:\d\d:\d\d,\d\d\d --> \d\d:\d\d:\d\d,\d\d\d/gm);
         let text = data.split(/\d\d:\d\d:\d\d,\d\d\d --> \d\d:\d\d:\d\d,\d\d\d/gm);
-        let srtArray = [];
     
         for (let i = 0; i < text.length - 1; i++) {
             srtArray.push([numberArray[i], timeSrt[i], text[i]]);
