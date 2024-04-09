ad2997cbba4bc91d6d87a263f5b3df610fdb2bdd
antonio <antoniopedroalvesdasilva.apads@gmail.com>
2024-04-09 19:18:52 -0300

    new update

--- my-configuration-files/js-scripts/addToMdTable.cjs
+++ my-configuration-files/js-scripts/addToMdTable.cjs
@@ -35,7 +35,7 @@ async function generateSrtArray(File) {
 // function that genetare a table with an collection of an Array
 
 function arrayToMdTable(Array,FirstRow){
-    if()
+    if(First)
     let table = `${FirstRow}`;
     Array.map((v,i,a)=>{
         
