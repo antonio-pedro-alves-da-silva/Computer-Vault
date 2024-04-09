9efbac12993f2dd75fc5f937cad8e2e78c39c7a7
antonio <antoniopedroalvesdasilva.apads@gmail.com>
2024-04-09 15:40:50 -0300

    new update

--- my-configuration-files/js-scripts/addToMdTable.js
+++ my-configuration-files/js-scripts/addToMdTable.js
@@ -6,7 +6,7 @@ const season = inputFile.match(/(?<=_s)\d+(?=e\d+.)/)[0];
 const episode = inputFile.match(/(?<=_s\de)\d+(?=[.])/)[0];
 
 
-
+const log = require()
 
 import { log } from "console";
 import { exec } from "child_process";
