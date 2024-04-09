d38d0eb6e669a60b523beaacbf15d79b95cb7664
antonio <antoniopedroalvesdasilva.apads@gmail.com>
2024-04-09 20:31:03 -0300

    new update

--- my-configuration-files/js-scripts/addToMdTable.cjs
+++ my-configuration-files/js-scripts/addToMdTable.cjs
@@ -42,7 +42,7 @@ function arrayToMdTable(ArrayList,FirstRow){
     });
 
     let restOfTheLines = '|';
-    ArrayList.forEach((v,i,a)=>{
+    ArrayList.map((v,i,a)=>{
         v.forEach((v2,i2,a2)=>{
             restOfTheLines += `${v2}|`
         })
