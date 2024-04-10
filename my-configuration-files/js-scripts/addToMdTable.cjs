97f4745e2715a3294e50ef76e211945909ad9eff
antonio <antoniopedroalvesdasilva.apads@gmail.com>
2024-04-09 21:18:30 -0300

    new update

--- my-configuration-files/js-scripts/addToMdTable.cjs
+++ my-configuration-files/js-scripts/addToMdTable.cjs
@@ -34,7 +34,7 @@ async function generateSrtArray(File) {
 }
 
 generateSrtArray(srtFile).then((result) => {
-    console.log("Promise resolved with result:", result);
+    
 }).catch((error) => {
     console.error("Promise rejected with error:", error);
 })
