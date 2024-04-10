1e769507f24c5ea9695f6a0b38cf14c56535e67c
antonio <antoniopedroalvesdasilva.apads@gmail.com>
2024-04-09 21:28:01 -0300

    new update

--- my-configuration-files/js-scripts/addToMdTable.cjs
+++ my-configuration-files/js-scripts/addToMdTable.cjs
@@ -21,6 +21,7 @@ async function generateSrtArray(File) {
         let text = data.split(/\d\d:\d\d:\d\d,\d\d\d --> \d\d:\d\d:\d\d,\d\d\d/gm);
     
         for (let i = 0; i < text.length - 1; i++) {
+            console.log()
             srtArray.push([numberArray[i], timeSrt[i], text[i]]);
         };
         return srtArray;
