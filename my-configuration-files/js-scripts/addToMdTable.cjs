63839b1ea837b07488020d8cbcad5c22ce64f905
antonio <antoniopedroalvesdasilva.apads@gmail.com>
2024-04-09 21:13:40 -0300

    new update

--- my-configuration-files/js-scripts/addToMdTable.cjs
+++ my-configuration-files/js-scripts/addToMdTable.cjs
@@ -33,7 +33,7 @@ async function generateSrtArray(File) {
 
 }
 
-console.log(generateSrtArray)
+console.log(generateSrtArray(srt))
 
 // function that generate a table with an collection of an Array |english|portuguese|audio|
 
