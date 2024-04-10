692d01698cd904388e9fa4f16641016399ac8742
antonio <antoniopedroalvesdasilva.apads@gmail.com>
2024-04-10 18:33:12 -0300

    new update 1182

---
+++ my-configuration-files/js-scripts/computer-vault-git-backup.cjs
@@ -0,0 +1,28 @@
+
+const { exec } = require('child_process');
+
+// Function to send notification
+function gitPush() {
+    // Execute the notify-send command
+    let command = 'cd ~/Computer-Vault && git add -A && git commit -m "new update $(git rev-list --count HEAD)" && git push -u ssh-origin main';
+    exec(command, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`Error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.error(`stderr: ${stderr}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+    });
+}
+
+// 
+
+setInterval(gitPush, 60*1000);
+
+
+
+
+
