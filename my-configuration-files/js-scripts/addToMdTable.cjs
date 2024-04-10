e81f0907153bcd17629c5af150b7fa3ad0879947
antonio <antoniopedroalvesdasilva.apads@gmail.com>
2024-04-09 21:22:17 -0300

    new update

--- my-configuration-files/js-scripts/addToMdTable.cjs
+++ my-configuration-files/js-scripts/addToMdTable.cjs
@@ -23,7 +23,7 @@ async function generateSrtArray(File) {
         for (let i = 0; i < text.length - 1; i++) {
             srtArray.push([numberArray[i], timeSrt[i], text[i]]);
         };
-        return srtArray;
+        consolesrtArray;
         
     } catch (err) {
         console.error(`Error reading file: ${err}`);
