7dde31f61256ea8dba961c9683b66347ffbdd34d
antonio <antoniopedroalvesdasilva.apads@gmail.com>
2024-04-09 21:35:16 -0300

    new update

--- my-configuration-files/js-scripts/addToMdTable.cjs
+++ my-configuration-files/js-scripts/addToMdTable.cjs
@@ -61,3 +61,10 @@ function arrayToMdTable(ArrayList,FirstRow){
 
 }
 
+function reverseString(string) {
+    let reversedString = "";
+    for (let i = string.length - 1; i >= 0; i--) {
+      reversedString += string[i];
+    }
+    return reversedString;
+  }
