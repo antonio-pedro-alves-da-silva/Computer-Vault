794a45530afc8d5dfe58f3f58561d4f8737ef5a3
antonio <antoniopedroalvesdasilva@gmail.com>
2024-04-17 21:46:56 -0300

    new update 50

--- my-configuration-files/js-scripts/audioCutter4.cjs
+++ my-configuration-files/js-scripts/audioCutter4.cjs
@@ -9,56 +9,71 @@ const episode = inputFile.match(/(?<=_s\de)\d+(?=[.])/)[0];
 
 
 const log = require("console");
-const {exec}= require("child_process");
+const { exec } = require("child_process");
 const fs = require("fs/promises");
+const {spawn} = require('child_process');
 
 
-
+let i = 0;
 async function audioCutter(subtitleFile) {
 
-    try {
-        const subtitleData = await fs.readFile(subtitleFile, "utf8");
-        let timingArr = subtitleData.match(/\d\d:\d\d:\d\d,\d\d\d --> \d\d:\d\d:\d\d,\d\d\d/gm);
-        let n_with_equalArr = subtitleData.match(/\d+(?=[=])/gm);
-        
+  try {
+    const subtitleData = await fs.readFile(subtitleFile, "utf8");
+    let timingArr = subtitleData.match(/\d\d:\d\d:\d\d,\d\d\d --> \d\d:\d\d:\d\d,\d\d\d/gm);
+    let n_with_equalArr = subtitleData.match(/\d+(?=[=])/gm);
 
-        for(i=0;i<(n_with_equalArr.length - 1);){
 
-          let time = timingArr[v].split("-->");
-          let startTime = time[0].trim().replace(",", ".");
-          let endTime = time[1].trim().replace(",", ".");
-          let c_cut = `ffmpeg -i ${inputFile} -vn -ss ${startTime} -to ${endTime} -q:a 0 ${seriesAndInfo}_${v}.mp3`;
 
-          exec(c_cut,(error)=>{
-            if(error){
-              console.log(error)
-            }
-            else if (i == (a.length - 1)){
-              console.log('all audios was cut')
-            }
-            else {
-              console.log(`corte concluido:${i}`);
-              n +=1
-            }
-              
-          })
+
+    n_with_equalArr.map(async function (v, i, a) {
+
+      let time = timingArr[v].split("-->");
+      let startTime = time[0].trim().replace(",", ".");
+      let endTime = time[1].trim().replace(",", ".");
+      let c_cut = `ffmpeg -i ${inputFile} -vn -ss ${startTime} -to ${endTime} -q:a 0 ${seriesAndInfo}_${v}.mp3`;
+      const childProcess = spawn(c_cut);
+      childProcess.on('close',(code)=>{
+        console.log(`audio cortado ${i}`)
+      })
+
+
+    })
+
+
+
+    /*exec(c_cut, (error, stdout) => {
+      if (error) {
+        console.log(error)
+      } else {
+        i += 1;
+        if (i == a.length - 1) {
+          console.log('all audio was cutted')
         }
+        else {
+          console.log(`audio was cut:${i}`)
+          audioCutter(subtitleFile);
 
-} catch (err) {
-        console.error(`Error reading file: ${err}`);
-        throw err; // Propagate the error
-    }
+        }
+      }
 
+    })*/
+
+
+  } catch (err) {
+    console.error(`Error reading file: ${err}`);
+    throw err; // Propagate the error
   }
-
-  audioCutter(subtitleFile);
-
-function removeScape(string){
-    return string.replace(/(\n)|(\r)|(\r\n)/gm,' ');
-    
 }
 
 
-function removeNumberAtTheEnd(string){
-   return string.replace(/(\d+\s+$)/,' ');
+audioCutter(subtitleFile);
+
+function removeScape(string) {
+  return string.replace(/(\n)|(\r)|(\r\n)/gm, ' ');
+
+}
+
+
+function removeNumberAtTheEnd(string) {
+  return string.replace(/(\d+\s+$)/, ' ');
 }
