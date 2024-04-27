626e477da2eff9f6e76ecbba7202b0171b116ce0
antonio <antoniopedroalvesdasilva@gmail.com>
2024-04-27 18:20:47 -0300

    new update 167

--- my-configuration-files/js-scripts/audioCutter4.cjs
+++ my-configuration-files/js-scripts/audioCutter4.cjs
@@ -4,7 +4,7 @@ const { exec } = require("child_process");
 const fs = require("fs/promises");
 
 
-const inputFile = "development_s1e7.mkv";
+const inputFile = "development_s1e8.mkv";
 const subtitleFile = "subtitles.srt";
 const translatedFile = "translated.srt";
 const seriesAndInfo = inputFile.split(".")[0];
