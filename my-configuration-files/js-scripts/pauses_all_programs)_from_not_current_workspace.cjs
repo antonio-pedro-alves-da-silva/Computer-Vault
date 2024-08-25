b42210d1cd3ff0c58509afa661246236039804b9
antonio <antoniopedroalvesdasilva.apads@gmail.com>
2024-08-25 15:35:22 -0300

    new update 5

--- my-configuration-files/js-scripts/pauses_all_programs)_from_not_current_workspace.cjs
+++ my-configuration-files/js-scripts/pauses_all_programs)_from_not_current_workspace.cjs
@@ -6,31 +6,32 @@ const { exec } = require("child_process");
 
 // Pause Others Programs
 
-let pop = `
+let gwi = `
 current_workspace=$(wmctrl -d | grep '*' | awk '{print $1}')
 all_windows_id="$(wmctrl -l | awk '{ if($2 != '$current_workspace') print $1;}' | tr '\n' ' ')"
 echo $all_windows_id
 `
-
-
 exec(gwi,(error,stdout,stderr)=>{
 	if(error){
 		console.log(error)
 	}
-	// get window pid
-	let gwp;
-	let wid = `${stdout}`.split(' ');
-
+	// instructions to get all window pid
+	let gwp = "";
+	let wid = `${stdout}`.replaceAll('\n','').split(" ");
 	wid.forEach((id)=>{
-		gwp = `xprop -id ${id} _NET_WM_PID`;
+		 gwp += `xprop -id ${id} _NET_WM_PID\n`
+	})
 
-		exec(gwp,(err,stdout,stderr)=>{
-			if(err){
-				console.log(err)
-			};
-			console.log(stdout)
+	exec(gwp,(error,stdout,stdeer)=>{
+		if(error){
+			console.log(error)
+		}
+		let pids_array = stdout.match(/\d+/gm);
+		pids_array.forEach((v)=>{
+			
 		})
 	})
+
 
 }) 
 
