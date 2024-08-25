70e7e12a5f785161ade60c7ce33f1489dcaa5ea8
antonio <antoniopedroalvesdasilva.apads@gmail.com>
2024-08-25 17:35:21 -0300

    new update 7

--- my-configuration-files/js-scripts/pauses_all_programs)_from_not_current_workspace.cjs
+++ my-configuration-files/js-scripts/pauses_all_programs)_from_not_current_workspace.cjs
@@ -20,20 +20,26 @@ exec(gwi,(error,stdout,stderr)=>{
 	let gwp = "";
 	let wid = `${stdout}`.replaceAll('\n','').split(" ");
 	wid.forEach((id,i,arr)=>{
-		 gwp += `xprop -id ${id} _NET_WM_PID\n`
+		 gwp += `xprop -id ${id} _NET_WM_PID\n`;
 	})
 
 	exec(gwp,(error,stdout,stdeer)=>{
 		if(error){
 			console.log(error)
 		}
-		// Pause Others Programs
-		let pop = ""; 
+			
 		pids_array = stdout.match(/\d+/gm);
 		// pause others programs
-		pids_array.forEach((v)=>{
-			pop =+ `kill -SIGKILL ${v}\n`;
+		pop = pids_array.map((v)=>{
+			return `pkill -KILL -P ${v}`;
+		}).join("\n")
+
+		exec(pop,(error,stdout)=>{
+			if(error){
+				console.log(error)
+			}
 		})
+		// resume others programs
 
 	})
 }) 
