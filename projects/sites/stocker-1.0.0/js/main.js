2783872682721a463378c4564ee75752689010df
robotUser <robotuser@pop-os.intelbras.local>
2024-09-15 10:23:42 -0300

    new update 11

--- projects/sites/stocker-1.0.0/js/main.js
+++ projects/sites/stocker-1.0.0/js/main.js
@@ -1,9 +1,13 @@
 
 $(window).ready(()=>{
-    $("#container-spinner").addClass("d-none")
+    $("#container-spinner").addClass("d-none");
 
     $(".btn-primary").on("click",()=>{
-        $(".btn-primary")
+        $(".btn-primary").css("box-shadow","0px 0px 0px 0.2rem rgba(0,208,132,0.6)")
+    })
+
+    $(".btn-primary").on("blur",()=>{
+        $(".btn-primary").css("box-shadow","0px 0px 0px 0px rgba(0,208,132,0.6)")
     })
 })
 
