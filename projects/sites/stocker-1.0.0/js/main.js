d475c739cedff48e96d4ad4e6a340b123914800c
robotUser <robotuser@pop-os.intelbras.local>
2024-09-16 15:11:21 -0300

    new update 26

--- projects/sites/stocker-1.0.0/js/main.js
+++ projects/sites/stocker-1.0.0/js/main.js
@@ -5,13 +5,18 @@
 $(window).ready(()=>{
     $("#container-spinner").addClass("d-none");
 
-    $(".btn-primary").on("click",()=>{
-        $(".btn-primary").css("box-shadow","0px 0px 0px 0.2rem rgba(0,208,132,0.6)")
+    // $(".btn-primary").on("click",()=>{
+    //     $(".btn-primary").css(`box-shadow`,`0px 0px 0px 0.2rem var(--bs-${}-rgb)`)
+    // })
+
+    // $(".btn-primary").on("blur",()=>{
+    //     $(".btn-primary").css(`box-shadow`,`0px 0px 0px 0px var(--bs-${}-rgb)`)
+    // })
+
+    $(window).on('click',(e)=>{
+        console.log(e.target);
     })
 
-    $(".btn-primary").on("blur",()=>{
-        $(".btn-primary").css("box-shadow","0px 0px 0px 0px rgba(0,208,132,0.6)")
-    })
 
     // setting owl carousel
     $('.owl-carousel').owlCarousel({
