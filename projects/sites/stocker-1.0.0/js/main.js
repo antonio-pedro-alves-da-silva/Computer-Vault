8fdb846d05a7de62a57e4820dac0c530a83c569c
robotUser <robotuser@pop-os.intelbras.local>
2024-09-16 22:13:18 -0300

    new update 33

--- projects/sites/stocker-1.0.0/js/main.js
+++ projects/sites/stocker-1.0.0/js/main.js
@@ -1,36 +1,43 @@
+$(window).ready(() => {
+  $("#container-spinner").addClass("d-none");
 
+  // $(".btn-primary").on("click",()=>{
+  //     $(".btn-primary").css(`box-shadow`,`0px 0px 0px 0.2rem var(--bs-${}-rgb)`)
+  // })
 
+  $(".btnShadowAnimation").on("click", (e) => {
+    btnShadowColor = $(e.target).attr("shadow-color");
+    $(e.target).css("box-shadow", `0px 0px 0px 0.2rem ${btnShadowColor}`);
+  });
 
+  $(".btnShadowAnimation").on("blur", (e) => {
+    btnShadowColor = $(e.target).attr("shadow-color");
+    $(e.target).css("box-shadow", `0px 0px 0px 0rem ${btnShadowColor}`);
+  });
 
-$(window).ready(()=>{
-    $("#container-spinner").addClass("d-none");
+  // setting owl carousel
+  var owl = $(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 0,
+    nav: false,
+    responsive: {
+      0: {
+        items: 1,
+      },
+    },
+  });
 
-    // $(".btn-primary").on("click",()=>{
-    //     $(".btn-primary").css(`box-shadow`,`0px 0px 0px 0.2rem var(--bs-${}-rgb)`)
-    // })
+  // setting the next and prev btn
+//   var owl = $(".owl-carousel");
+//   owl.owlCarousel();
 
-    
-    $(".btnShadowAnimation").on('click',(e)=>{
-        btnShadowColor = $(e.target).attr("shadow-color");
-        $(e.target).css("box-shadow",`0px 0px 0px 0.2rem ${btnShadowColor}`)
-    })
-    
-    $(".btnShadowAnimation").on('blur',(e)=>{
-        btnShadowColor = $(e.target).attr("shadow-color");
-        $(e.target).css("box-shadow",`0px 0px 0px 0rem ${btnShadowColor}`)
-    })
-    
-    // setting owl carousel
-    $('.owl-carousel').owlCarousel({
-        loop:true,
-        margin:0,
-        nav:true,
-        responsive:{
-            0:{
-                items:1
-            }
-        }
-    })
+  $(".nextBtn").click(function () {
+    owl.trigger("next.owl.carousel");
+  });
 
-})
-
+  $(".prevBtn").click(function () {
+    // With optional speed parameter
+    // Parameters has to be in square bracket '[]'
+    owl.trigger("prev.owl.carousel", [300]);
+  });
+});
