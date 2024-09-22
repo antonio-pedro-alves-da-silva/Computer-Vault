26776bd4e98a90b0a1e940fcac998181bc0cbf8a
robotUser <robotuser@pop-os.intelbras.local>
2024-09-22 14:30:25 -0300

    new update 84

--- projects/sites/stocker-1.0.0/js/main.js
+++ projects/sites/stocker-1.0.0/js/main.js
@@ -110,13 +110,24 @@ cards.on("click",(e)=>{
   var blogCarousel = $(".blog-carousel").owlCarousel({
     loop: true,
     margin: 0,
-    nav: false,
+    nav: true,
     responsive: {
       0: {
         items: 1,
       },
     },
-
   });
+
+  // Go to the next item
+  $('.customNextBtn').click(function() {
+    owl.trigger('next.owl.carousel');
+  })
+
+  // Go to the previous item
+  $('.customPrevBtn').click(function() {
+    // With optional speed parameter
+    // Parameters has to be in square bracket '[]'
+    owl.trigger('prev.owl.carousel', [300]);
+  })
 
 });
