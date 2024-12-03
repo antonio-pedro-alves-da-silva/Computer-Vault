817ec260ba1189950a05d5ec68e16d4b604b0fe0
antonio pedro <antoniopedroalvesdasilva.apads@gmail.com>
2024-12-03 17:13:57 -0300

    new commit 149

---
+++ 9.1+Authentication+Lv.1/queries.sql
@@ -0,0 +1,20 @@
+
+/*
+CREATE TABLE users(
+	id BIGSERIAL NOT NULL,
+	email VARCHAR(100) NOT NULL UNIQUE NOT NULL,
+	password VARCHAR(100) NOT NULL,
+	PRIMARY KEY(id)
+);
+*/
+/*
+insert into users (email,password)
+values
+	('3d','fajskl'),
+	('ddal','jaks');
+*/
+
+select * from users;
+
+
+
