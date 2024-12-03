86aaa6a92c11893248c255705b24daf765019b14
antonio pedro <antoniopedroalvesdasilva.apads@gmail.com>
2024-12-03 18:27:20 -0300

    new commit 150

--- Lista de Tarefas/lista de tarefas.md
+++
@@ -1,7 +0,0 @@
-
-- [ ] tirar o lixo
-- [ ] tirar o frango do congelador
-- [ ] tirar o feijão do congelador
-- [ ] limpar a sanduicheira
-- [ ] tirar o gelo do congelador
-
--- echo.sh
+++
@@ -1,7 +0,0 @@
-output=$(xset q | grep -o "Num Lock:\s*\(off\|on\)")
-if [[ "$output" == "Num Lock:    off" ]]; then
-  echo -e "\x1b[31m$output\x1b[0m"  # Red if Num Lock is off
-else
-  echo -e "\x1b[32m$output\x1b[0m"  # Green if Num Lock is on
-fi
-
