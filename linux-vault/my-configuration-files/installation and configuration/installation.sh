0add7853b5f61c8208ccf70b6274622a0e1b9765
Antonio Pedro <86069266+antonio-pedro-alves-da-silva@users.noreply.github.com>
2024-12-04 20:11:52 -0300

    changing apt-fast to apt-get

--- linux-vault/my-configuration-files/installation and configuration/installation.sh
+++ linux-vault/my-configuration-files/installation and configuration/installation.sh
@@ -10,27 +10,27 @@ xclip -sel clip < ~/.ssh/id_ed25519.pub
 sudo apt update
 sudo add-apt-repository ppa:apt-fast/stable
 sudo apt install apt-fast -y
-sudo apt-fast install xvkbd -y
+sudo apt-get install xvkbd -y
 
 git clone --depth=1 https://github.com/AdnanHodzic/auto-cpufreq.git
 cd auto-cpufreq && sudo ./auto-cpufreq-installer
 sudo auto-cpufreq --install
 sudo auto-cpufreq --force=performance
 
-sudo apt-fast install curl -y
-sudo apt-fast install snapd -y
+sudo apt-get install curl -y
+sudo apt-get install snapd -y
 # instalação do tmux(não terminado)
 
 # instalação do zsh
 
-sudo apt-fast install zsh -y
+sudo apt-get install zsh -y
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/plugins/zsh-autosuggestions
 
-sudo apt-fast install fonts-powerline -y
+sudo apt-get install fonts-powerline -y
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/themes/powerlevel10k
 
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
@@ -39,24 +39,24 @@ cd ~/.fzf/
 
 # instalaçao do i3
 
-sudo apt-fast install feh -y
-sudo apt-fast install xdotool -y
+sudo apt-get install feh -y
+sudo apt-get install xdotool -y
 sudo snap install node --classic
-sudo apt-fast install flameshot -y
-sudo apt-fast install i3 -y
-sudo apt-fast install i3status -y
-sudo apt-fast install blueman -y
-sudo apt-fast install tilix -y
-sudo apt-fast install brightnessctl
-sudo apt-fast install i3blocks -y
+sudo apt-get install flameshot -y
+sudo apt-get install i3 -y
+sudo apt-get install i3status -y
+sudo apt-get install blueman -y
+sudo apt-get install tilix -y
+sudo apt-get install brightnessctl
+sudo apt-get install i3blocks -y
 
 # other packages 
 
-sudo apt-fast install gparted -y
-sudo apt-fast install ffmpeg -y
-sudo apt-fast install pavucontrol -y
-sudo apt-fast install pulseaudio -y
-sudo apt-fast install nautilus -y
+sudo apt-get install gparted -y
+sudo apt-get install ffmpeg -y
+sudo apt-get install pavucontrol -y
+sudo apt-get install pulseaudio -y
+sudo apt-get install nautilus -y
 sudo snap install code --classic
 sudo npm install -g nodemon
 
@@ -64,10 +64,10 @@ sudo npm install -g nodemon
 # download mysql-apt-config and run dpkg
 sudo apt-get update
 
-sudo apt-fast install mysql-server -y
+sudo apt-get install mysql-server -y
 sudo sudo mysql_secure_installation
 
 sudo apt-get update
-sudo apt-fast install mysql-workbench-community -y
+sudo apt-get install mysql-workbench-community -y
 
 
